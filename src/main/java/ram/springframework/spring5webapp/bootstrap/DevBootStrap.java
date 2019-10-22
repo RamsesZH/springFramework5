6ae488d1332c3834a2f28e61c857b1f7d64266f4
Ramses Znagui Hassani <ramses.znagui@gmail.com>
2019-10-22 13:33:46 +0200

    Sprint Framework Events

---
+++ src/main/java/ram/springframework/spring5webapp/bootstrap/DevBootStrap.java
@@ -0,0 +1,49 @@
+package ram.springframework.spring5webapp.bootstrap;
+
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import ram.springframework.spring5webapp.model.Author;
+import ram.springframework.spring5webapp.model.Book;
+import ram.springframework.spring5webapp.repositories.AuthorRepository;
+import ram.springframework.spring5webapp.repositories.BookRepository;
+
+@Component
+public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    private void initData(){
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design","1234","Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development with no EJB","23344","Worx");
+        rod.getBooks().add(noEJB);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+
+
+}
